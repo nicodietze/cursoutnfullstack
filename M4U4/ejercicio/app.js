9f52927710545f8a9bed9aa39228c703b36e8872
Nico Dietze <nicodietze@hotmail.com>
2022-03-20 12:09:23 -0300

    Ejercicio m4u4

--- M4U4/ejercicio/app.js
+++ M4U4/ejercicio/app.js
@@ -48,6 +48,27 @@ app.get('/salir', function (req, res) {
   res.redirect('/');
 });
 
+app.use(function(req, res, next) {
+  if (!req.session.vistas) {
+    req.session.vistas ={};
+  }
+
+if (!req.session.vistas[req.originalUrl]) {
+  req.session.vistas[req.originalUrl] = 1;
+} else {
+  req.session.vistas[req.originalUrl]++;
+}
+next();
+});
+
+app.get('/pagina1', function(req, res) {
+  res.render('pagina', {
+    nombre:'Contador de vistas',
+    vistas: req.session.vistas[req.originalUrl]
+  })
+})
+
+
 /* app.use('/', indexRouter);
 app.use('/users', usersRouter); */
 
