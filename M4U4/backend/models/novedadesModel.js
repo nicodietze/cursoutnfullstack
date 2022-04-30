93d618cd01b7e45b74f304856918ee89a1dd7622
Nico Dietze <nicodietze@hotmail.com>
2022-04-30 19:40:26 -0300

    m6u1 clase1

---
+++ M4U4/backend/models/novedadesModel.js
@@ -0,0 +1,21 @@
+var pool = require('./bd');
+
+async function getNovedades() {
+    var query = "select * from novedades order by id desc";
+    var rows = await pool.query(query);
+    return rows;
+}
+
+async function insertNovedad(obj) {
+    try {
+        var query="insert into novedades set ? ";
+        var rows= await pool.query(query, [obj]);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+module.exports= { getNovedades, insertNovedad }
+//module.exports= { getNovedades, deleteNovedadById, insertNovedad }
