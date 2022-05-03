29c9a4f3642081d37c34e2ac299db5c7300b99bb
Nico Dietze <nicodietze@hotmail.com>
2022-05-03 17:58:48 -0300

    Actividad M6U2

--- M4U4/backend/models/novedadesModel.js
+++ M4U4/backend/models/novedadesModel.js
@@ -8,8 +8,8 @@ async function getNovedades() {
 
 async function insertNovedad(obj) {
     try {
-        var query="insert into novedades set ? ";
-        var rows= await pool.query(query, [obj]);
+        var query = "insert into novedades set ? ";
+        var rows = await pool.query(query, [obj]);
         return rows;
     } catch (error) {
         console.log(error);
@@ -17,5 +17,29 @@ async function insertNovedad(obj) {
     }
 }
 
-module.exports= { getNovedades, insertNovedad }
-//module.exports= { getNovedades, deleteNovedadById, insertNovedad }
+async function deleteNovedadById(id) {
+    var query = "delete from novedades qhere id = ?";
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
+
+async function getNovedadById(id) {
+    var query = "select * from novedades where id = ? ";
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+async function modificarNovedadByID(obj, id) {
+    try {
+        var query = "update novedades set ? where id=?";
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = {
+    getNovedades, insertNovedad, deleteNovedadById,
+    getNovedadById, modificarNovedadByID
+}
--- M4U4/backend/routes/admin/novedades.js
+++ M4U4/backend/routes/admin/novedades.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var usuariosModel = require('./../../models/usuariosModel');
-var novedadesModel = require ('./../../models/novedadesModel');
+var novedadesModel = require('./../../models/novedadesModel');
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
     var novedades = await novedadesModel.getNovedades();
-    res.render('admin/novedades', { layout: 'admin/layout',
-usuario: req.session.nombre,
-novedades
- });
+    res.render('admin/novedades', {
+        layout: 'admin/layout',
+        usuario: req.session.nombre,
+        novedades
+    });
 });
 
 router.get('/agregar', (req, res, next) => {
@@ -21,22 +22,57 @@ router.get('/agregar', (req, res, next) => {
 router.post('/agregar', async (req, res, next) => {
     try {
         if (req.body.titulo != "" && req.body.subtitulo != "" &&
-        req.body.cuerpo != "") {
+            req.body.cuerpo != "" && req.body.enlace != "") {
             await novedadesModel.insertNovedad(req.body);
             res.redirect('/admin/novedades')
-            } else {
-                res.render('admin/agregar', {
-                    layout:'admin/layout',
-                    error: true, message: 'Todos los campos son requeridos'
-                    })
-            }
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true, message: 'Todos los campos son requeridos'
+            })
+        }
     } catch (error) {
         console.log(error)
-        res.render ('admin/agregar', {
+        res.render('admin/agregar', {
             layout: 'admin/layout',
             error: true, message: 'No se cargo la novedad'
         });
     }
+});
+
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadById(id);
+    res.redirect('/admin/novedades')
+});
+
+router.get('/modificar/:id', async (req, res, next) => {
+    let id = req.params.id;
+    let novedad = await novedadesModel.getNovedadById(id);
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedad
+    });
+});
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+        let obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo,
+            enlace: req.body.enlace,
+        }
+        await novedadesModel.modificarNovedadByID(obj, req.body.id);
+        res.redirect('/admin/novedades');
+    }
+    catch (error) {
+        console.log(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true, message: 'No se pudo modificar la novedad'
+        });
+    };
 });
 
 module.exports = router;
